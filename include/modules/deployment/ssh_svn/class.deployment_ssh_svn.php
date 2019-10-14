8d7e749e4727924e72193ff98aac76d991dfb478
A Codeweavers Infrastructure Bod <36475663+infraweavers@users.noreply.github.com>
2019-10-14 15:42:30 +0100

    Update class.deployment_ssh_svn.php

--- include/modules/deployment/ssh_svn/class.deployment_ssh_svn.php
+++ include/modules/deployment/ssh_svn/class.deployment_ssh_svn.php
@@ -5,11 +5,11 @@ class ssh_svn extends NConf_Deployment_Modules
         $this->path = dirname(__FILE__);
     }
     public function command($host_infos){
-
-        // execute rsync
-		$command = "ssh " . $host_infos["ssh_options"] . " " . $host_infos["username"] . "@" . $host_infos["host"] . " 'sudo /usr/bin/svn update --username " . $host_infos["svnUsername"] . " --password " . $host_infos["svnPassword"] . " --non-interactive --no-auth-cache " . $host_infos["path"] . "'";
-        $status = $this->system_call($command, TRUE);
-        
+		$status = array();
+		foreach($host_infos["host"] as $host){
+			$command = "ssh " . $host_infos["ssh_options"] . " " . $host_infos["username"] . "@" . $host_infos["host"] . " 'sudo /usr/bin/svn update --username " . $host_infos["svnUsername"] . " --password " . $host_infos["svnPassword"] . " --non-interactive --no-auth-cache " . $host_infos["path"] . "'";
+			$status[] = $this->system_call($command, TRUE);
+		}
 		return $status;
     }
 }
