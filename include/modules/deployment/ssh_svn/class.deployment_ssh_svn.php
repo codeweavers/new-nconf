75bf664937d2f98913d9fe636bcb854bba373032
A Codeweavers Infrastructure Bod <36475663+infraweavers@users.noreply.github.com>
2019-10-14 14:15:46 +0100

    Update class.deployment_ssh_svn.php

--- include/modules/deployment/ssh_svn/class.deployment_ssh_svn.php
+++ include/modules/deployment/ssh_svn/class.deployment_ssh_svn.php
@@ -7,7 +7,7 @@ class ssh_svn extends NConf_Deployment_Modules
     public function command($host_infos){
 
         // execute rsync
-		$command = "ssh " . $host_infos["ssh_options"] . " " . $host_infos["username"] . "@" . $host_infos["host"] . " 'svn update --username=\"" . $host_infos["svnUsername"] . "\" --password=\"" . $host_infos["svnPassword"] . "\" --non-interactive \"" . $host_infos["path"] . "\"'";
+		$command = "ssh " . $host_infos["ssh_options"] . " " . $host_infos["username"] . "@" . $host_infos["host"] . " 'svn update --username=". '"' . $host_infos["svnUsername"] . '"' . " --password=" . $host_infos["svnPassword"] . " --non-interactive " . $host_infos["path"] . "'";
         $status = $this->system_call($command, TRUE);
         
 		return $status;
