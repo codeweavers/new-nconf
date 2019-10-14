96bdcfff6bd600a61e51f9b0b1913bcaf87bf0dc
A Codeweavers Infrastructure Bod <36475663+infraweavers@users.noreply.github.com>
2019-10-14 12:21:54 +0100

    Create class.deployment_ssh_svn.php

---
+++ include/modules/deployment/ssh_svn/class.deployment_ssh_svn.php
@@ -0,0 +1,16 @@
+<?php
+class ssh_svn extends NConf_Deployment_Modules
+{
+    function __construct(){
+        $this->path = dirname(__FILE__);
+    }
+    public function command($host_infos){
+
+        // execute rsync
+		$command = "ssh ". $host_infos["$username"] . "@" . $host_infos["$host"] . " 'svn update --username=\"" . $host_infos["$svnUsername"] . "\" --password=\"" . $host_infos["$svnPassword"] . "\" --non-interactive \"" . $host_infos["$path"] . "\"'";
+        $status = $this->system_call($command, TRUE);
+        
+		return $status;
+    }
+}
+?>
