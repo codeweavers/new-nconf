15197345f40a6c9fd9ff486e4516192d1e822cd2
A Codeweavers Infrastructure Bod <36475663+infraweavers@users.noreply.github.com>
2019-12-19 12:53:00 +0000

    Update class.deployment_ssh_svn.php

--- include/modules/deployment/ssh_svn/class.deployment_ssh_svn.php
+++ include/modules/deployment/ssh_svn/class.deployment_ssh_svn.php
@@ -5,8 +5,9 @@ class ssh_svn extends NConf_Deployment_Modules
         $this->path = dirname(__FILE__);
     }
     public function command($host_infos){
-		$command = "ssh " . $host_infos["ssh_options"] . " " . $host_infos["username"] . "@" . $host_infos["host"] . " 'sudo /usr/bin/svn update --username " . $host_infos["svnUsername"] . " --password " . $host_infos["svnPassword"] . " --non-interactive --no-auth-cache " . $host_infos["path"] . "'";
-		$status = $this->system_call($command, TRUE);
+	    $command = "ssh " . $host_infos["ssh_options"] . " " . $host_infos["username"] . "@" . $host_infos["host"] . " 'sudo /usr/bin/svn update --username " . $host_infos["svnUsername"] . " --password " . $host_infos["svnPassword"] . " --non-interactive --no-auth-cache " . $host_infos["path"] . "'";
+	    $status = $this->system_call($command, TRUE);
+	    return $status;
     }
 }
 ?>
